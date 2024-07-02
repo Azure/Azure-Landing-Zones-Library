5271467de89e03a5a3763eaeefe8e44ebb226e1d
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-07-02 01:15:41 +0100

    feat: policy sync

--- .github/scripts/Invoke-LibraryUpdatePolicyAssignmentArchetypes.ps1
+++ .github/scripts/Invoke-LibraryUpdatePolicyAssignmentArchetypes.ps1
@@ -7,11 +7,11 @@
 
 [CmdletBinding(SupportsShouldProcess)]
 param (
-  [Parameter()][String]$AlzToolsPath = "$PWD/enterprise-scale/src/Alz.Tools",
-  [Parameter()][String]$TargetPath = "$PWD/alzlib",
-  [Parameter()][String]$SourcePath = "$PWD/enterprise-scale",
-  [Parameter()][String]$LineEnding = "unix",
-  [Parameter()][String]$ParserToolUrl = "https://github.com/jaredfholgate/template-parser/releases/download/0.1.18"
+    [Parameter()][String]$AlzToolsPath = "$PWD/enterprise-scale/src/Alz.Tools",
+    [Parameter()][String]$TargetPath = "$PWD/library",
+    [Parameter()][String]$SourcePath = "$PWD/enterprise-scale",
+    [Parameter()][String]$LineEnding = "unix",
+    [Parameter()][String]$ParserToolUrl = "https://github.com/Azure/arm-template-parser/releases/download/0.2.4"
 )
 
 $ErrorActionPreference = "Stop"
@@ -21,32 +21,20 @@ $ErrorActionPreference = "Stop"
 # module.
 Import-Module $AlzToolsPath -ErrorAction Stop
 
-$parserPath = "$TargetPath/.temp/scripts"
+$parserPath = "$TargetPath/.github/scripts"
 $parserExe = "Template.Parser.Cli"
 if ($IsWindows) {
-  $parserExe += ".exe"
+    $parserExe += ".exe"
 }
 
 $parser = "$parserPath/$parserExe"
 
 if (!(Test-Path $parser)) {
-  Write-Information "Downloading Template Parser." -InformationAction Continue
-  New-Item -ItemType Directory -Path $parserPath -Force | Out-Null
-  Invoke-WebRequest "$ParserToolUrl/$parserExe" -OutFile $parser
-  if (! $IsWindows) {
-    $isExecutable = $(test -x "$parser"; 0 -eq $LASTEXITCODE)
-    if (!($isExecutable)) {
-      chmod +x $parser
+    Write-Information "Downloading Template Parser." -InformationAction Continue
+    Invoke-WebRequest "$ParserToolUrl/$parserExe" -OutFile $parser
+    if ($IsLinux -Or $IsMacOS) {
+        chmod +x $parser
     }
-  }
-}
-
-$EmptyArchetype = @{
-  "name"                   = "name"
-  "policy_assignments"     = @()
-  "policy_definitions"     = @()
-  "policy_set_definitions" = @()
-  "role_definitions"       = @()
 }
 
 # Update the policy assignments if enabled
@@ -60,77 +48,200 @@ $eslzArm = & $parser "-s $eslzArmSourcePath" "-f $eslzArmParametersSourcePath" "
 $policyAssignments = New-Object 'System.Collections.Generic.Dictionary[string,System.Collections.Generic.List[string]]'
 
 foreach ($resource in $eslzArm) {
-  $scope = $resource.scope
-  $policyAssignment = $resource.properties.templateLink.uri
+    $scope = $resource.scope
+    $policyAssignment = $resource.properties.templateLink.uri
 
-  if ($null -ne $policyAssignment -and $policyAssignment.StartsWith("https://deploymenturi/managementGroupTemplates/policyAssignments/") -and $resource.condition) {
-    $managementGroup = $scope.Split("/")[-1]
-    $policyAssignmentFileName = $policyAssignment.Split("/")[-1]
+    if ($null -ne $policyAssignment -and $policyAssignment.StartsWith("https://deploymenturi/managementGroupTemplates/policyAssignments/") -and $resource.condition) {
+        $managementGroup = $scope.Split("/")[-1]
+        $policyAssignmentFileName = $policyAssignment.Split("/")[-1]
 
-    if (!($policyAssignmentFileName.StartsWith("fairfax"))) {
-      if (!($policyAssignments.ContainsKey($managementGroup))) {
-        $values = New-Object 'System.Collections.Generic.List[string]'
-        $values.Add($policyAssignmentFileName)
-        $policyAssignments.Add($managementGroup, $values)
-      }
-      else {
-        $policyAssignments[$managementGroup].Add($policyAssignmentFileName)
-      }
+        if (!($policyAssignmentFileName.StartsWith("fairfax"))) {
+            if (!($policyAssignments.ContainsKey($managementGroup))) {
+                $values = New-Object 'System.Collections.Generic.List[string]'
+                $values.Add($policyAssignmentFileName)
+                $policyAssignments.Add($managementGroup, $values)
+            }
+            else {
+                $policyAssignments[$managementGroup].Add($policyAssignmentFileName)
+            }
+        }
     }
-  }
 }
 
 $managementGroupMapping = @{
-  "defaults"       = "root"
-  "management"     = "management"
-  "connectivity"   = "connectivity"
-  "corp"           = "corp"
-  "landingzones"   = "landing_zones"
-  "decommissioned" = "decommissioned"
-  "sandboxes"      = "sandboxes"
-  "identity"       = "identity"
-  "platform"       = "platform"
+    "defaults"       = "root"
+    "management"     = "management"
+    "connectivity"   = "connectivity"
+    "corp"           = "corp"
+    "landingzones"   = "landing_zones"
+    "decommissioned" = "decommissioned"
+    "sandboxes"      = "sandboxes"
+    "identity"       = "identity"
+    "platform"       = "platform"
+}
+
+$logAnalyticsWorkspaceIdPlaceholder = "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/placeholder/providers/Microsoft.OperationalInsights/workspaces/placeholder-la"
+
+$parameters = @{
+    default   = @{
+        nonComplianceMessagePlaceholder          = "{donotchange}"
+        logAnalyticsWorkspaceName                = "placeholder-la"
+        automationAccountName                    = "placeholder-automation"
+        workspaceRegion                          = "uksouth"
+        automationRegion                         = "uksouth"
+        retentionInDays                          = "30"
+        rgName                                   = "`${root_scope_id}-mgmt"
+        logAnalyticsResourceId                   = "$logAnalyticsWorkspaceIdPlaceholder"
+        topLevelManagementGroupPrefix            = "`${temp}"
+        dnsZoneResourceGroupId                   = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Network/privateDnsZones/"
+        ddosPlanResourceId                       = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Network/ddosProtectionPlans/placeholder"
+        emailContactAsc                          = "security_contact@replace_me"
+        location                                 = "uksouth"
+        listOfResourceTypesDisallowedForDeletion = "[[[Array]]]"
+        userWorkspaceResourceId                  = "$logAnalyticsWorkspaceIdPlaceholder"
+        userAssignedIdentityResourceId           = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.ManagedIdentity/userAssignedIdentities/placeholder"
+        dcrResourceId                            = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Insights/dataCollectionRules/placeholder"
+        dataCollectionRuleResourceId             = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Insights/dataCollectionRules/placeholder"
+    }
+    overrides = @{
+        sql_data_collection_rule_overrides             = @{
+            policy_assignments = @(
+                "DINE-MDFCDefenderSQLAMAPolicyAssignment.json"
+            )
+            parameters         = @{
+                dcrResourceId                = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Insights/dataCollectionRules/placeholder"
+                dataCollectionRuleResourceId = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Insights/dataCollectionRules/placeholder"
+            }
+        }
+        vm_insights_data_collection_rule_overrides     = @{
+            policy_assignments = @(
+                "DINE-VMHybridMonitoringPolicyAssignment.json",
+                "DINE-VMMonitoringPolicyAssignment.json",
+                "DINE-VMSSMonitoringPolicyAssignment.json"
+            )
+            parameters         = @{
+                dcrResourceId                = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Insights/dataCollectionRules/placeholder"
+                dataCollectionRuleResourceId = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Insights/dataCollectionRules/placeholder"
+            }
+        }
+        change_tracking_data_collection_rule_overrides = @{
+            policy_assignments = @(
+                "DINE-ChangeTrackingVMArcPolicyAssignment.json",
+                "DINE-ChangeTrackingVMPolicyAssignment.json",
+                "DINE-ChangeTrackingVMSSPolicyAssignment.json"
+            )
+            parameters         = @{
+                dcrResourceId                = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Insights/dataCollectionRules/placeholder"
+                dataCollectionRuleResourceId = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Insights/dataCollectionRules/placeholder"
+            }
+        }
+    }
 }
 
 $finalPolicyAssignments = New-Object 'System.Collections.Generic.Dictionary[string,System.Collections.Generic.List[string]]'
 
 $policyAssignmentSourcePath = "$SourcePath/eslzArm/managementGroupTemplates/policyAssignments"
-
-
+$policyAssignmentTargetPath = "$TargetPath/platform/alz/policy_assignments"
 
 foreach ($managementGroup in $policyAssignments.Keys) {
-  foreach ($policyAssignmentFile in $policyAssignments[$managementGroup]) {
-    $parsedAssignment = & $parser "-s $policyAssignmentSourcePath/$policyAssignmentFile" | Out-String | ConvertFrom-Json
-    $policyAssignmentName = $parsedAssignment.name
-
     $managementGroupNameFinal = $managementGroupMapping[$managementGroup.Replace("defaults-", "")]
+    Write-Output "`nProcessing Archetype Policy Assignments for Management Group: $managementGroupNameFinal"
 
-    Write-Information "Got final data for $managementGroupNameFinal and $policyAssignmentName" -InformationAction Continue
+    foreach ($policyAssignmentFile in $policyAssignments[$managementGroup]) {
+        Write-Output "`nProcessing Archetype Policy Assignment: $managementGroupNameFinal - $policyAssignmentFile"
 
-    if (!($finalPolicyAssignments.ContainsKey($managementGroupNameFinal))) {
-      $values = New-Object 'System.Collections.Generic.List[string]'
-      $values.Add($policyAssignmentName)
-      $finalPolicyAssignments.Add($managementGroupNameFinal, $values)
+        $defaultParameters = $parameters.default
+        foreach ($overrideKey in $parameters.overrides.Keys) {
+            if ($policyAssignmentFile -in $parameters.overrides[$overrideKey].policy_assignments) {
+                foreach ($parameter in $parameters.overrides[$overrideKey].parameters.Keys) {
+                    $defaultParameters.$parameter = $parameters.overrides[$overrideKey].parameters.$parameter
+                }
+            }
+        }
+
+        $defaultParameterFormatted = $defaultParameters.GetEnumerator().ForEach({ "-p $($_.Name)=$($_.Value)" })
+
+        $parsedAssignmentArray = & $parser "-s $policyAssignmentSourcePath/$policyAssignmentFile" $defaultParameterFormatted "-a" | Out-String | ConvertFrom-Json
+
+        foreach ($parsedAssignment in $parsedAssignmentArray) {
+            if ($parsedAssignment.type -ne "Microsoft.Authorization/policyAssignments") {
+                continue
+            }
+
+            $policyAssignmentName = $parsedAssignment.name
+
+            Write-Output "Parsed Assignment Name: $($parsedAssignment.name)"
+
+            if (!(Get-Member -InputObject $parsedAssignment.properties -Name "scope" -MemberType Properties)) {
+                $parsedAssignment.properties | Add-Member -MemberType NoteProperty -Name "scope" -Value "/providers/Microsoft.Management/managementGroups/placeholder"
+            }
+
+            if (!(Get-Member -InputObject $parsedAssignment.properties -Name "notScopes" -MemberType Properties)) {
+                $parsedAssignment.properties | Add-Member -MemberType NoteProperty -Name "notScopes" -Value @()
+            }
+
+            if (!(Get-Member -InputObject $parsedAssignment.properties -Name "parameters" -MemberType Properties)) {
+                $parsedAssignment.properties | Add-Member -MemberType NoteProperty -Name "parameters" -Value @{}
+            }
+
+            if (!(Get-Member -InputObject $parsedAssignment -Name "location" -MemberType Properties)) {
+                $parsedAssignment | Add-Member -MemberType NoteProperty -Name "location" -Value "uksouth"
+            }
+
+            # if (!(Get-Member -InputObject $parsedAssignment -Name "identity" -MemberType Properties)) {
+            #     $parsedAssignment | Add-Member -MemberType NoteProperty -Name "identity" -Value @{ type = "None" }
+            # }
+
+            if ($parsedAssignment.properties.policyDefinitionId.StartsWith("/providers/Microsoft.Management/managementGroups/`${temp}")) {
+                $parsedAssignment.properties.policyDefinitionId = $parsedAssignment.properties.policyDefinitionId.Replace("/providers/Microsoft.Management/managementGroups/`${temp}", "/providers/Microsoft.Management/managementGroups/placeholder")
+            }
+
+            foreach ($property in Get-Member -InputObject $parsedAssignment.properties.parameters -MemberType NoteProperty) {
+                $propertyName = $property.Name
+                Write-Verbose "Checking Parameter: $propertyName"
+                if ($parsedAssignment.properties.parameters.($propertyName).value.GetType() -ne [System.String]) {
+                    Write-Verbose "Skipping non-string parameter: $propertyName"
+                    continue
+                }
+
+                if ($parsedAssignment.properties.parameters.($propertyName).value.StartsWith("`${private_dns_zone_prefix}/providers/Microsoft.Network/privateDnsZones/")) {
+                    $parsedAssignment.properties.parameters.($propertyName).value = $parsedAssignment.properties.parameters.($propertyName).value.Replace("`${private_dns_zone_prefix}/providers/Microsoft.Network/privateDnsZones/", "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/placeholder/providers/Microsoft.Network/privateDnsZones/")
+                    # $parsedAssignment.properties.parameters.($propertyName).value = $parsedAssignment.properties.parameters.($propertyName).value.Replace("privatelink.uks.backup.windowsazure.com", "privatelink.`${connectivity_location_short}.backup.windowsazure.com")
+                }
+                if ($parsedAssignment.properties.parameters.($propertyName).value.StartsWith("`${temp}")) {
+                    $parsedAssignment.properties.parameters.($propertyName).value = $parsedAssignment.properties.parameters.($propertyName).value.Replace("`${temp}", "/providers/Microsoft.Management/managementGroups/placeholder")
+                }
+            }
+
+            $targetPolicyAssignmentFileName = "$($policyAssignmentName.ToLower() -replace "-", "_").alz_policy_assignment.json"
+
+            Write-Information "Writing $targetPolicyAssignmentFileName" -InformationAction Continue
+            $json = $parsedAssignment | ConvertTo-Json -Depth 10
+            $json | Edit-LineEndings -LineEnding $LineEnding | Out-File -FilePath "$policyAssignmentTargetPath/$targetPolicyAssignmentFileName" -Force
+
+            Write-Verbose "Got final data for $managementGroupNameFinal and $policyAssignmentName"
+
+            if (!($finalPolicyAssignments.ContainsKey($managementGroupNameFinal))) {
+                $values = New-Object 'System.Collections.Generic.List[string]'
+                $values.Add($policyAssignmentName)
+                $finalPolicyAssignments.Add($managementGroupNameFinal, $values)
+            }
+            else {
+                $finalPolicyAssignments[$managementGroupNameFinal].Add($policyAssignmentName)
+            }
+        }
     }
-    else {
-      $finalPolicyAssignments[$managementGroupNameFinal].Add($policyAssignmentName)
-    }
-  }
 }
 
-$policyAssignmentTargetPath = "$TargetPath/archetype_definitions"
+$policyAssignmentTargetPath = "$TargetPath/modules/archetypes/lib/archetype_definitions"
 
 foreach ($managementGroup in $finalPolicyAssignments.Keys) {
-  $archetypeFilePath = "$policyAssignmentTargetPath/$managementGroup.alz_archetype_definition.json"
-  if (!(Test-Path $archetypeFilePath)) {
-    $ThisArchetype = $EmptyArchetype.Clone()
-    $ThisArchetype.name = $managementGroup
-    $ThisArchetype | ConvertTo-Json | Edit-LineEndings -LineEnding $LineEnding | Out-File -FilePath "$archetypeFilePath" -Force
-  }
-  $archetypeJson = Get-Content $archetypeFilePath | ConvertFrom-Json
-  $archetypeJson.policy_assignments = @($finalPolicyAssignments[$managementGroup] | Sort-Object)
+    $archetypeFilePath = "$policyAssignmentTargetPath/$managementGroup.alz_archetype_definition.json"
+    $archetypeJson = Get-Content $archetypeFilePath | ConvertFrom-Json
 
-  Write-Information "Writing $archetypeFilePath" -InformationAction Continue
-  $json = $archetypeJson | ConvertTo-Json -Depth 10
-  $json | Edit-LineEndings -LineEnding $LineEnding | Out-File -FilePath "$archetypeFilePath" -Force
+    $archetypeJson.policy_assignments = @($finalPolicyAssignments[$managementGroup] | Sort-Object)
+
+    Write-Information "Writing $archetypeFilePath" -InformationAction Continue
+    $json = $archetypeJson | ConvertTo-Json -Depth 10
+    $json | Edit-LineEndings -LineEnding $LineEnding | Out-File -FilePath "$archetypeFilePath" -Force
 }
