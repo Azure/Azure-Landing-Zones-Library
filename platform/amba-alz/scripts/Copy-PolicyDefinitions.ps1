74097a7758e7a7335bca2cdc9df3ab6b241c5ae1
Arjen Huitema <arjenhuitema@microsoft.com>
2024-09-18 14:29:46 +0200

    fix: add Copy-PolicyDefinitions script to automate policy file copying

---
+++ platform/amba-alz/scripts/Copy-PolicyDefinitions.ps1
@@ -0,0 +1,23 @@
+
+[CmdletBinding(SupportsShouldProcess)]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplatePath,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SourcePath,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TargetPath
+)
+
+$files = (Select-String -Path $TemplatePath/policies-*.bicep -Pattern "../../../services/").Line -replace ([regex]::Escape("loadTextContent('../../..")), "" -replace ([regex]::Escape("')")), ""
+
+New-Item $TargetPath -Type Directory
+
+foreach ($file in $files) {
+    Copy-Item -Path $SourcePath/$file -Destination $TargetPath -Force
+}
