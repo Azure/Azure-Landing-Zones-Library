efefe60aa50201c32ca16710b91b0a983b194c6e
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-01-09 11:01:14 +0000

    fix: incorrect comment in script

--- scripts/check-lib.sh
+++ scripts/check-lib.sh
@@ -5,14 +5,13 @@ set -eo pipefail
 # The first argument passed to the script, representing the library directory
 LIB="$1"
 
-echo "==> building docs with alzlibtool \"$LIB\"..."
+echo "==> checking library with alzlibtool \"$LIB\"..."
 # Check if alzlibtool is installed
 if ! command -v alzlibtool &> /dev/null; then
     echo "alzlibtool could not be found. Please install it and ensure it is in your PATH."
     exit 1
 fi
 
-echo "==> checking library with alzlibtool \"$LIB\"..."
 # does the $LIB directory exist?
 if [ ! -d "$LIB" ]; then
     echo "==> \"$LIB\" does not exist. Exiting..."
